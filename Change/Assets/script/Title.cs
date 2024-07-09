9c378f4ddb2fcf3e13a9629a41dc6ef47e7ccc85
wataraikashu <wataraikashu@gmail.com>
2024-07-09 15:58:11 +0900

    (update)Title画面を選択できるように

--- Change/Assets/script/Title.cs
+++ Change/Assets/script/Title.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Title : MonoBehaviour
 {
+
+    [SerializeField]
+    private Selectable firstSelected = null;
+
+
+    void Start()
+    {
+        firstSelected.Select();
+    }
+
     public void LoadNextScene()
     {
         SceneManager.LoadScene("game1");
